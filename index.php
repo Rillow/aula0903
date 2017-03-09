e3a719938c766157a6789ef4732c4b5b677c82ab
Rillow <rodrigo.rodrigues11@etec.sp.gov.br>
2017-03-09 15:08:00 -0300

    PHP completo

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+$v1=$_GET['valor1'];
+$v2=$_GET['valor2'];
+$total=$v1+$v2;
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -11,7 +20,8 @@
         Valor 1<input type="text" name="valor1">
         Valor 2<input type="text" name="valor2">
         <button type="submit" class="btn btn-success">Enviar</button>
-        
+
+        <?= $total ?>
 
         </form>
     </body>
